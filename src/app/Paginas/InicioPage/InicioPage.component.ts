fb63aa74f96aaa3b6cab42485ab6c77037ef56a0
PLDev <pedroluisrp1998@gmail.com>
2024-09-20 19:14:06 +0200

    Info de pantalla incio unida con la APi PLDEV

--- src/app/Paginas/InicioPage/InicioPage.component.ts
+++ src/app/Paginas/InicioPage/InicioPage.component.ts
@@ -1,12 +1,27 @@
 import {Component} from '@angular/core';
+import {DatosInicioService} from "../../Services/DatosInicio/datos-inicio.service";
+import {HomePage} from "../../Services/Models/DatosInicio";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-InicioPage',
   templateUrl: './InicioPage.component.html',
   styleUrls: ['./InicioPage.component.css'],
+  imports: [
+    NgForOf
+  ],
   standalone: true
 })
 export class InicioPageComponent {
-  constructor() {
+  public datosInicio: HomePage|null = null;
+  protected readonly JSON = JSON;
+
+  constructor(private datosInicioService: DatosInicioService) {}
+
+  ngOnInit() {
+    this.datosInicioService.getDatosInicio().subscribe(
+      data => this.datosInicio = data
+    )
   }
+
 }
---
+++ src/app/Services/Models/DatosInicio.ts
@@ -0,0 +1,47 @@
+// Definición de las interfaces
+export interface HomePage {
+  home: Home;
+}
+
+export interface Home {
+  header: Header;
+  introduction: Introduction;
+  skills: Skills;
+  cta: ActionLink[]; // Ahora cta es un array de ActionLink
+  projects: Projects;
+}
+
+export interface Header {
+  title: string;
+  subtitle: string;
+}
+
+export interface Introduction {
+  paragraph: string;
+}
+
+export interface Skills {
+  title: string;
+  list: Skill[];
+}
+
+export interface Skill{
+  text: string;
+  class: string;
+}
+
+export interface ActionLink {
+  text: string;
+  url: string;
+}
+
+export interface Projects {
+  title: string;
+  list: Project[];
+}
+
+export interface Project {
+  name: string;
+  description: string;
+  technologies: string[];
+}
